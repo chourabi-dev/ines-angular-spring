69d123aaaa9005264a4674b8e73a1e08f93028e5
chourabi-dev <chourabitahers62@gmail.com>
2022-08-16 15:41:42 +0100

    latest

---
+++ spring/SQL-JPA - DAY 7/inesdemo/src/main/java/com/inesdemo/requests/ProductRequest.java
@@ -0,0 +1,37 @@
+package com.inesdemo.requests;
+
+public class ProductRequest {
+
+	
+	private String title;
+	private float price;
+	private int quantity;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public ProductRequest() {
+		super();
+	}
+	
+	
+	
+	
+	
+}
---
+++ spring/SQL-JPA - DAY 7/inesdemo/src/main/java/com/inesdemo/responses/JsonResponse.java
@@ -0,0 +1,27 @@
+package com.inesdemo.responses;
+
+public class JsonResponse {
+
+	private boolean success;
+	private String message;
+	
+	
+	
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public JsonResponse() {
+		super();
+	}
+	
+	
+}
